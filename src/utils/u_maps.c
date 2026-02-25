9304f6ec54a30323429f72a0940dda62e04f1866
CDaureo <cdaureo08@gmail.com>
2026-02-25 12:58:42 +0100

    U_maps done

--- src/utils/u_maps.c
+++ src/utils/u_maps.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   u_maps.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: simgarci <simgarci@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cdaureo- <cdaureo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 14:15:39 by cdaureo-          #+#    #+#             */
-/*   Updated: 2026/02/19 14:05:17 by simgarci         ###   ########.fr       */
+/*   Updated: 2026/02/25 12:58:23 by cdaureo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,54 +16,55 @@
  * Valida si un carácter es permitido en el mapa
  * Caracteres válidos: '0', '1', ' ', '\t', 'N', 'S', 'E', 'W',, D
  */
-int is_allow_chars(char c)
+int	is_allow_chars(char c)
 {
-	return (c == '0' || c == '1' || c == ' ' || c == '\t' ||
-			c == 'N' || c == 'S' || c == 'E' || c == 'W' || c == 'D');
+	return (c == '0' || c == '1' || c == ' ' || c == '\t'
+		|| c == 'N' || c == 'S' || c == 'E' || c == 'W' || c == 'D');
 }
+
 /**
  * Valida que todos los caracteres de una línea de mapa sean permitidos
  * Ignora saltos de línea (\n, \r)
  * Retorna 1 si válida, 0 si contiene caracteres no permitidos
  */
-int sanitize_row(const char *row)
+int	sanitize_row(const char *row)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (!row)
-		return(0);
-	
+		return (0);
 	while (row[i])
 	{
 		if (row[i] == '\n' || row[i] == '\r')
 		{
 			i++;
-			continue;
+			continue ;
 		}
 		if (!is_allow_chars(row[i]))
-			return(0);
+			return (0);
 		i++;
 	}
 	return (1);
 }
+
 /**
  * Añade una nueva fila al mapa y expande el array dinámicamente
  * Actualiza la altura y el ancho máximo del mapa
  */
-int push_row(t_map *map, char *row_dup)
+int	push_row(t_map *map, char *row_dup)
 {
 	char	**new_row;
 	int		i;
-	
+
 	i = 0;
 	new_row = (char **)malloc(sizeof(char *) * (map->height + 2));
 	if (!new_row)
-		return(printf("Error: Not enought memory.\n"));
+		return (printf("Error: Not enought memory.\n"));
 	while (i < map->height)
 	{
 		new_row[i] = map->rows[i];
-			i++;
+		i++;
 	}
 	new_row[i++] = row_dup;
 	new_row[i] = NULL;
@@ -75,24 +76,26 @@ int push_row(t_map *map, char *row_dup)
 	return (1);
 }
 
-int get_map_value(t_map *map, int x, int y, t_game *game)
+int	get_map_value(t_map *map, int x, int y, t_game *game)
 {
+	char	cell;
+	t_door	*door;
+
 	if (x < 0 || y < 0 || x >= map->width || y >= map->height)
 		return (1);
-	
-	char cell = map->rows[y][x];
-	
+	cell = map->rows[y][x];
 	if (cell == '1')
 		return (1);
 	else if (cell == 'D')
 	{
-		t_door *door = find_door(game, x, y);
+		door = find_door(game, x, y);
 		if (door && door->is_open)
 			return (0);
 		else
 			return (1);
 	}
-	else if (cell == '0' || cell == 'N' || cell == 'S' || cell == 'E' || cell == 'W')
+	else if (cell == '0' || cell == 'N'
+		|| cell == 'S' || cell == 'E' || cell == 'W')
 		return (0);
 	else
 		return (1);
--- src/utils/u_minimap.c
+++ src/utils/u_minimap.c
@@ -5,14 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cdaureo- <cdaureo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/18 17:02:01 by simgarci          #+#    #+#             */
-/*   Updated: 2026/02/25 12:42:07 by cdaureo-         ###   ########.fr       */
+/*   Created: 2026/02/25 12:50:39 by cdaureo-          #+#    #+#             */
+/*   Updated: 2026/02/25 12:53:45 by cdaureo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-void init_minimap(t_minimap *minimap, t_mlx *mlx)
+void	init_minimap(t_minimap *minimap, t_mlx *mlx)
 {
 	minimap->map_start_x = screenWidth - MINIMAP_SIZE - MINIMAP_OFFSET;
 	minimap->map_start_y = screenHeight - MINIMAP_SIZE - MINIMAP_OFFSET;
@@ -21,29 +21,30 @@ void init_minimap(t_minimap *minimap, t_mlx *mlx)
 	minimap->radius = MINIMAP_SIZE / 2;
 	minimap->player_pixel_x = minimap->center_x;
 	minimap->player_pixel_y = minimap->center_y;
-	minimap->player_angle = atan2(mlx->dirY, mlx->dirX) + M_PI/2;
+	minimap->player_angle = atan2(mlx->dirY, mlx->dirX) + M_PI / 2;
 	minimap->arrow_length = 6;
 	minimap->arrow_width = 4;
 }
 
-static void draw_dot(t_mlx *mlx, int dot_size, int center_x, int center_y)
+static void	draw_dot(t_mlx *mlx, int dot_size, int center_x, int center_y)
 {
-	int y;
-	int x;
-	int pixel_x;
-	int pixel_y;
+	int	y;
+	int	x;
+	int	pixel_x;
+	int	pixel_y;
 
 	y = -dot_size;
-	while(y <= dot_size)
+	while (y <= dot_size)
 	{
 		x = -dot_size;
-		while(x <= dot_size)
+		while (x <= dot_size)
 		{
-			if(x*x + y*y <= dot_size*dot_size)
+			if (x * x + y * y <= dot_size * dot_size)
 			{
 				pixel_x = center_x + x;
 				pixel_y = center_y + y;
-				if(pixel_x >= 0 && pixel_y >= 0 && pixel_x < screenWidth && pixel_y < screenHeight)
+				if (pixel_x >= 0 && pixel_y >= 0
+					&& pixel_x < screenWidth && pixel_y < screenHeight)
 				{
 					ft_mlx_pixel_put(mlx, pixel_x, pixel_y, 0xFFFFFF);
 				}
@@ -54,27 +55,34 @@ static void draw_dot(t_mlx *mlx, int dot_size, int center_x, int center_y)
 	}
 }
 
-void draw_crosshair(t_mlx *mlx)
+static void	draw_crosshair_outline(t_mlx *mlx, t_minimap *map)
 {
-	t_minimap map;
+	if (map->x * map->x + map->y * map->y
+		<= map->outline_size * map->outline_size)
+	{
+		map->pixel_x = map->center_x + map->x;
+		map->pixel_y = map->center_y + map->y;
+		if (map->pixel_x >= 0 && map->pixel_y >= 0
+			&& map->pixel_x < screenWidth && map->pixel_y < screenHeight)
+			ft_mlx_pixel_put(mlx, map->pixel_x, map->pixel_y, 0x000000);
+	}
+}
+
+void	draw_crosshair(t_mlx *mlx)
+{
+	t_minimap	map;
 
 	map.center_x = screenWidth / 2;
 	map.center_y = screenHeight / 2;
 	map.dot_size = 2;
 	map.outline_size = map.dot_size + 1;
 	map.y = -map.outline_size;
-	while(map.y <= map.outline_size)
+	while (map.y <= map.outline_size)
 	{
-		while(map.x <= map.outline_size)
+		while (map.x <= map.outline_size)
 		{
 			map.x = -map.outline_size;
-			if(map.x*map.x + map.y*map.y <= map.outline_size*map.outline_size)
-			{
-				map.pixel_x = map.center_x + map.x;
-				map.pixel_y = map.center_y + map.y;
-				if(map.pixel_x >= 0 && map.pixel_y >= 0 && map.pixel_x < screenWidth && map.pixel_y < screenHeight)
-					ft_mlx_pixel_put(mlx, map.pixel_x, map.pixel_y, 0x000000);
-			}
+			draw_crosshair_outline(mlx, &map);
 			map.x++;
 		}
 		map.y++;
