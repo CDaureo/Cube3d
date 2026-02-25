3d6143cc3dcb32dd8b3bb996d55231adf5f981de
CDaureo <cdaureo08@gmail.com>
2026-02-25 12:39:12 +0100

    u_rendering done

--- src/utils/u_rendering.c
+++ src/utils/u_rendering.c
@@ -3,24 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   u_rendering.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: simgarci <simgarci@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cdaureo- <cdaureo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 14:06:57 by simgarci          #+#    #+#             */
-/*   Updated: 2026/02/19 14:08:11 by simgarci         ###   ########.fr       */
+/*   Updated: 2026/02/25 12:38:48 by cdaureo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-long get_time_ms(void)
+long	get_time_ms(void)
 {
-	struct timeval tv;
+	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
 	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
-int error_checker(t_game *game, int argc, char **argv)
+int	error_checker(t_game *game, int argc, char **argv)
 {
 	if (argc != 2)
 	{
@@ -42,7 +42,7 @@ int error_checker(t_game *game, int argc, char **argv)
 	return (0);
 }
 
-void clear_image(t_mlx *data)
+void	clear_image(t_mlx *data)
 {
-	memset(data->addr, 0, screenHeight * data->line_length);
+	ft_memset(data->addr, 0, screenHeight * data->line_length);
 }
